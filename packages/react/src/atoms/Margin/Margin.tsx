d80004b2735b336f2d7c7816925efcd76557ea22
Srikar charan <srikarsuri@gmail.com>
2022-11-01 15:54:01 +0530

    fix(docs): update comments in packages
    long desc

--- packages/react/src/atoms/Margin/Margin.tsx
+++ packages/react/src/atoms/Margin/Margin.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Spacing } from "@ds.e/foundation";
+import { Spacing } from "@srikar-ds.e/foundation";
 
 export interface MarginProps {
   space?: keyof typeof Spacing;
